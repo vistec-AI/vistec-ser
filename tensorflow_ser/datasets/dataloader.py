577a65ca1d9828663122eb7062da5b91d96527ac
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2020-12-30 11:46:08 +0700

    fix dataloader and add augmentations

---
+++ tensorflow_ser/datasets/dataloader.py
@@ -0,0 +1,84 @@
+from typing import *
+
+import tensorflow as tf
+import numpy as np
+
+from .features import load_waveform, FeatureLoader
+from augmentations.augmentation import Augmentation
+
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+TFRECORD_SHARDS = 16
+
+
+class DataLoader:
+    def __init__(self,
+                 csv_paths: Union[List[str], str],
+                 feature_loader: FeatureLoader,
+                 augmentations: Augmentation = Augmentation(None),
+                 shuffle: bool = True):
+        if isinstance(csv_paths, str):
+            csv_paths = [csv_paths]
+        self.csv_paths = csv_paths
+        self.shuffle = shuffle
+        self.total_steps = None
+        self.feature_loader = feature_loader
+        self.augmentations = augmentations
+
+    def read_csv(self):
+        lines = list()
+        for csv in self.csv_paths:
+            print(f"Reading {csv}...")
+            with tf.io.gfile.GFile(csv, 'r') as f:
+                lines = f.read().splitlines()[1:] # skip header
+        lines = np.array([line.split(',') for line in lines]) # line i: audio_path, emotion
+        if self.shuffle:
+            np.random.shuffle(lines)
+        self.total_steps = len(lines)
+        return lines
+
+    def get_dataset(self, batch_size):
+        data = self.read_csv()
+        if len(data) == 0:
+            return None
+        dataset = tf.data.Dataset.from_tensor_slices(data)
+        return self.process_dataset(dataset, batch_size=batch_size)
+
+    def preprocess(self, audio_path, emotion) -> Tuple[tf.Tensor, str]:
+        with tf.device('/CPU:0'):
+            waveform = load_waveform(audio_path)
+
+            # apply augmentation and feature extraction
+            waveform = self.augmentations.wave_augment.augment(waveform.numpy())
+            features = self.feature_loader.extract(waveform)
+            features = self.augmentations.feat_augment.augment(features)
+            features = tf.convert_to_tensor(features)
+
+            return features, emotion
+
+    @tf.function
+    def parse(self, data):
+        return tf.numpy_function(
+            self.preprocess,
+            inp=[data[0], data[1]],
+            Tout=[tf.float32, tf.string]
+        )
+
+    def process_dataset(self, dataset: tf.data.Dataset, batch_size: int) -> tf.data.Dataset:
+        dataset = dataset.map(self.parse, num_parallel_calls=AUTOTUNE)
+
+        if self.shuffle:
+            dataset = dataset.shuffle(TFRECORD_SHARDS, reshuffle_each_iteration=True)
+
+        dataset = dataset.padded_batch(
+            batch_size=batch_size,
+            padded_shapes=(
+                tf.TensorShape(self.feature_loader.shape),
+                tf.TensorShape([])
+            ),
+            padding_values=(0., None),
+            drop_remainder=True
+        )
+
+        dataset = dataset.prefetch(AUTOTUNE)
+        return dataset
--- tensorflow_ser/datasets/features.py
+++ tensorflow_ser/datasets/features.py
@@ -1,21 +1,11 @@
-import os
+from typing import *
 
 import tensorflow as tf
 
 
 def load_waveform(audio_path: str, sample_rate: int = 16000, bit_depth: int = 16, dither: float = 1.) -> tf.Tensor:
-    if not isinstance(audio_path, str):
-        raise TypeError('Audio path `{}` should be string'.format(audio_path))
-    if not os.path.exists(audio_path):
-        raise FileNotFoundError('Audio path `{}` not found'.format(audio_path))
     audio_binary = tf.io.read_file(audio_path)
     waveform, sr = tf.audio.decode_wav(audio_binary)
-    if sr != sample_rate:
-        raise ValueError('Sample rate mismatch. Expected {} got {}'.format(sample_rate, sr))
-    if waveform.shape[-1] != 1:
-        raise ValueError(
-            'Only mono channel supported. Your audio `{}` contains {} channels, make sure all files are '
-            'mono.'.format(audio_path, waveform.shape[-1]))
     waveform = tf.squeeze(waveform, axis=-1)
     return waveform + tf.random.normal(tf.shape(waveform), stddev=(dither / 2 ** bit_depth))
 
@@ -35,7 +25,7 @@ def normalize_feature(feature: tf.Tensor, scaled: bool = False, epsilon: float =
 
 
 class FeatureLoader:
-    def __init__(self, config: dict):
+    def __init__(self, config: Dict[str, object]):
         self.sample_rate = config.get('sample_rate', 16000)
         self.frame_length = int(self.sample_rate * (config.get("frame_length", 25) / 1000))
         self.frame_shift = int(self.sample_rate * (config.get("frame_shift", 10) / 1000))
@@ -44,22 +34,35 @@ class FeatureLoader:
         self.feature_type = config.get('feature_type', 'fbank')
         self.preemphasis = config.get('preemphasis', 0.97)
 
-        self.scale_normalize = config.get('scale_normalize', False)
+        self.scale_normalize = config.get('scale_normalize', True)
+
+    @property
+    def shape(self) -> List[int]:
+        if self.feature_type == 'fbank':
+            return [None, self.num_mel_bins]
+        else:
+            return [None, None]
 
     def stft(self, waveform: tf.Tensor) -> tf.Tensor:
-        return tf.square(
-            tf.abs(
+        return tf.abs(
                 tf.signal.stft(
                     waveform,
                     frame_length=self.frame_length,
-                    frame_step=self.frame_shift
+                    frame_step=self.frame_shift,
+                    window_fn=tf.signal.hann_window,
+                    pad_end=True
                 )
             )
-        )
+
+    def dbscale(self, spectrogram: tf.Tensor, top_db: int=80):
+        power = tf.math.square(spectrogram)
+        log_spec = 10.0 * (tf.math.log(power) / tf.math.log(10.0))
+        log_spec = tf.math.maximum(log_spec, tf.math.reduce_max(log_spec) - top_db)
+        return log_spec
 
     def make_spectrogram(self, waveform: tf.Tensor, epsilon: float = 1e-8) -> tf.Tensor:
         spectrogram = self.stft(waveform)
-        return tf.math.log(spectrogram + epsilon)
+        return self.dbscale(spectrogram)
 
     def make_fbank(self, waveform: tf.Tensor, fmin: float=0., fmax: float = 8000., epsilon: float = 1e-8) -> tf.Tensor:
         spectrogram = self.stft(waveform)
@@ -70,25 +73,22 @@ class FeatureLoader:
             lower_edge_hertz=fmin, upper_edge_hertz=fmax
         )
         fbank = tf.tensordot(spectrogram, mel_fbank, 1)
-        return tf.math.log(fbank + epsilon)
+        return self.dbscale(fbank)
 
     def make_mfcc(self, waveform: tf.Tensor) -> tf.Tensor:
         log_mel_spectrogram = self.make_fbank(waveform)
         return tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)
 
-    def extract(self, audio_path: str) -> tf.Tensor:
-        waveform = load_waveform(audio_path, sample_rate=self.sample_rate)
+    def extract(self, waveform: tf.Tensor) -> tf.Tensor:
         waveform = pre_emphasis(waveform, coeff=self.preemphasis)
 
-        if self.feature_type == "wave":
-            return waveform
-        elif self.feature_type == "spectrogram":
+        if self.feature_type == "spectrogram":
             features = self.make_spectrogram(waveform)
         elif self.feature_type == "fbank":
             features = self.make_fbank(waveform)
         elif self.feature_type == "mfcc":
             features = self.make_mfcc(waveform)
         else:
-            raise KeyError("Unsupported feature type `{}`. Only `wave`, `spectrogram`, `fbank`, and `mfcc` available.".format(self.feature_type))
+            raise KeyError("Unsupported feature type `{}`. Only `spectrogram`, `fbank`, and `mfcc` available.".format(self.feature_type))
 
         return normalize_feature(features, scaled=self.scale_normalize)
