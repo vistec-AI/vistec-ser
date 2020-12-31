e7d99470fa2a61cee15b0958fd204351215f2b89
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2020-12-31 22:37:14 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = [
 
 setuptools.setup(
     name="vistec-ser",
-    version="0.1a",
+    version="0.1a1",
     author="Chompakorn Chaksangchaichot",
     author_email="chompakorn.cc@gmail.com",
     description="Speech Emotion Recognition models and training using Tensorflow 2.x",
