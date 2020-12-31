4d4426b4dba520d4b051ba4899b9d01accc25e34
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2020-12-31 20:11:50 +0700

    refactor packages name

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# TensorflowSER
+# VistecSER
+<img alt="python" src="https://img.shields.io/badge/python-%3E%3D3.8-blue?logo=python">
+<img alt="tensorflow" src="https://img.shields.io/badge/tensorflow-%3E%3D2.3.0-orange?logo=tensorflow">
+
+Speech Emotion Recognition Model and Inferencing using Tensorflow 2.x
 
 ## Reference
 This repository was structured based on [TensorflowASR](https://github.com/TensorSpeech/TensorFlowASR) repository.
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+requirements = [
+    "tensorflow>=2.4.0",
+    "tensorflow_io>=0.17",
+    "librosa>=0.8.0",
+    "nlpaug>=1.1.1",
+]
+
+setuptools.setup(
+    name="VistecSER",
+    version="0.1a",
+    author="Chompakorn Chaksangchaichot",
+    author_email="chompakorn.cc@gmail.com",
+    description="Speech Emotion Recognition models and training using Tensorflow 2.x",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tann9949/VistecSER',
+    packages=setuptools.find_packages(include=['vistec_ser*']),
+    install_requires=requirements,
+    classifiers=[
+
+    ],
+    python_requires='>=3.8'
+)
