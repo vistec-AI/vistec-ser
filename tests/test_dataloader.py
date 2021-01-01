1f95c450a0efaa15fd635d14f0e68b721788ccc0
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2021-01-01 18:30:32 +0700

    fix test file

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,11 +1,15 @@
 from vistec_ser.datasets import DataLoader, FeatureLoader
 from vistec_ser.utils.config import Config
+import os
 
 CONFIG_PATH = 'tests/config.yml'
 CSV_PATH = 'tests/samples/lables.csv'
 
 
 def main():
+    assert os.path.exists(CONFIG_PATH), print(f"Config path does not exists in {os.getcwd()}")
+    assert os.path.exists(CSV_PATH), print(f"CSV path does not exists in {os.getcwd()}")
+
     config = Config(path=CONFIG_PATH)
     feature_loader = FeatureLoader(config=config.feature_config)
     data_loader = DataLoader(feature_loader=feature_loader, csv_paths=CSV_PATH, augmentations=config.augmentations)
