e31b440a63c8a3c39a20fc099f55ae270b55cae0
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2021-01-01 19:04:29 +0700

    fix CI pipelines

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -6,8 +6,8 @@ import os
 
 def main(argv):
     config_path, csv_path = argv[1], argv[2]
-    assert os.path.exists(config_path), f"Config path `{config_path}` does not exists in {os.getcwd()}"
-    assert os.path.exists(csv_path), f"CSV path `{csv_path}` does not exists in {os.getcwd()}"
+    assert os.path.exists(config_path), f"Config path `{config_path}` does not exists."
+    assert os.path.exists(csv_path), f"CSV path `{csv_path}` does not exists."
 
     config = Config(path=config_path)
     feature_loader = FeatureLoader(config=config.feature_config)
