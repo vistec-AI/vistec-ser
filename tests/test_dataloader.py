45106227654b50b7995f35e213b08a517e61df82
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2021-01-01 18:38:39 +0700

    fix test file

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,22 +1,21 @@
 from vistec_ser.datasets import DataLoader, FeatureLoader
 from vistec_ser.utils.config import Config
+import sys
 import os
 
-CONFIG_PATH = 'tests/config.yml'
-CSV_PATH = 'tests/samples/lables.csv'
 
+def main(argv):
+    config_path, csv_path = argv[1], argv[2]
+    assert os.path.exists(config_path), print(f"Config path does not exists in {os.getcwd()}")
+    assert os.path.exists(csv_path), print(f"CSV path does not exists in {os.getcwd()}")
 
-def main():
-    assert os.path.exists(CONFIG_PATH), print(f"Config path does not exists in {os.getcwd()}")
-    assert os.path.exists(CSV_PATH), print(f"CSV path does not exists in {os.getcwd()}")
-
-    config = Config(path=CONFIG_PATH)
+    config = Config(path=config_path)
     feature_loader = FeatureLoader(config=config.feature_config)
-    data_loader = DataLoader(feature_loader=feature_loader, csv_paths=CSV_PATH, augmentations=config.augmentations)
+    data_loader = DataLoader(feature_loader=feature_loader, csv_paths=csv_path, augmentations=config.augmentations)
     dataset = data_loader.get_dataset(batch_size=2)
     for x, y in dataset:
         print(x.shape, y)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
