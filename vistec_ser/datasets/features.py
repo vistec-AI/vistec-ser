1930400125a6b07dd50d9f1489f7d432e28bcad0
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2021-01-01 21:11:10 +0700

    refactor wildcard imports

--- vistec_ser/datasets/features.py
+++ vistec_ser/datasets/features.py
@@ -1,4 +1,4 @@
-from typing import *
+from typing import List, Dict, Any
 
 import tensorflow as tf
 
--- vistec_ser/utils/__init__.py
+++ vistec_ser/utils/__init__.py
@@ -1,1 +1,1 @@
-from .utils import *
+from .utils import float_feature, int64_feature, bytestring_feature, print_one_line
