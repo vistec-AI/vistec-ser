fce2e6736d18ce745260ee6a500666f24c8e330b
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2021-01-01 18:14:27 +0700

    fix test file

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -8,7 +8,7 @@ CSV_PATH = 'tests/samples/lables.csv'
 def main():
     config = Config(path=CONFIG_PATH)
     feature_loader = FeatureLoader(config=config.feature_config)
-    data_loader = DataLoader(feature_loader=feature_loader, csv_paths=csv_path, augmentations=config.augmentations)
+    data_loader = DataLoader(feature_loader=feature_loader, csv_paths=CSV_PATH, augmentations=config.augmentations)
     dataset = data_loader.get_dataset(batch_size=2)
     for x, y in dataset:
         print(x.shape, y)
