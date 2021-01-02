a4c12a0d6649a9a0402b671d42c38c2bb6901519
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2021-01-02 22:18:38 +0700

    fix unit test script

--- tests/test_training.py
+++ tests/test_training.py
@@ -14,17 +14,23 @@ def main(argv):
 
     config = Config(path=config_path)
     feature_loader = FeatureLoader(config=config.feature_config)
-    train_loader = DataLoader(feature_loader=feature_loader, csv_paths=train_csv_path,
-                              augmentations=config.augmentations)
+    train_loader = DataLoader(feature_loader=feature_loader, csv_paths=csv_path,augmentations=config.augmentations)
     train_dataset = train_loader.get_dataset(batch_size=batch_size)
-    validation_steps = len(train_dataset)
+    steps_per_epoch = train_loader.steps_per_epoch
 
-    val_loader = DataLoader(feature_loader=feature_loader, csv_paths=val_csv_path)
+    val_loader = DataLoader(feature_loader=feature_loader, csv_paths=csv_path)
     val_dataset = val_loader.get_dataset(batch_size=batch_size)
+    validation_steps = val_loader.steps_per_epoch
 
     model = TestModel()
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics='accuracy')
-    model.fit(train_dataset, validation_data=val_dataset, validation_steps=validation_steps, epochs=1)
+    model.fit(
+        train_dataset,
+        validation_data=val_dataset,
+        steps_per_epoch=steps_per_epoch,
+        validation_steps=validation_steps,
+        epochs=1
+    )
 
 
 if __name__ == '__main__':
