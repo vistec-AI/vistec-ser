fca539af63a5a2e0b0da8f154eca49b209e531e1
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2021-01-02 22:13:10 +0700

    fix unit test script/config

--- tests/test_training.py
+++ tests/test_training.py
@@ -10,15 +10,21 @@ def main(argv):
     assert os.path.exists(config_path), f"Config path `{config_path}` does not exists."
     assert os.path.exists(csv_path), f"CSV path `{csv_path}` does not exists."
 
+    batch_size = 2
+
     config = Config(path=config_path)
     feature_loader = FeatureLoader(config=config.feature_config)
-    train_loader = DataLoader(feature_loader=feature_loader, csv_paths=csv_path, augmentations=config.augmentations)
-    train_dataset = train_loader.get_dataset(batch_size=2)
-    val_loader = DataLoader(feature_loader=feature_loader, csv_paths=csv_path)
-    val_dataset = val_loader.get_dataset(batch_size=2)
+    train_loader = DataLoader(feature_loader=feature_loader, csv_paths=train_csv_path,
+                              augmentations=config.augmentations)
+    train_dataset = train_loader.get_dataset(batch_size=batch_size)
+    validation_steps = len(train_dataset)
+
+    val_loader = DataLoader(feature_loader=feature_loader, csv_paths=val_csv_path)
+    val_dataset = val_loader.get_dataset(batch_size=batch_size)
+
     model = TestModel()
-    model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    model.fit(train_dataset, validation_data=val_dataset, epochs=1)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics='accuracy')
+    model.fit(train_dataset, validation_data=val_dataset, validation_steps=validation_steps, epochs=1)
 
 
 if __name__ == '__main__':
